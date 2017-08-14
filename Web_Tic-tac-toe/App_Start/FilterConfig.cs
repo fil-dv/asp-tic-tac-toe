fdc5f7c3d5fa382ef17cbfced9e2a60702a991d2
D.Filonenko <D.Filonenko@FILONENKODV>
2017-08-14 15:19:12 +0300

    Add project files.

---
+++ Web_Tic-tac-toe/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Web_Tic_tac_toe
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
